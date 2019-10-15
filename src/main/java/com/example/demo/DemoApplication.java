acd88ae5f419954a5032b5e6aceac87bde454e57
ToruTamahashi <0ys25x4t725473s@gmail.com>
2019-10-15 13:18:48 +0900

    commit4

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Controller
 public class DemoApplication {
-
-    @Autowired
-    private MessageService service;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showTopPage(){
